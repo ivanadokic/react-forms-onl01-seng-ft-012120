351b71ad61b286d73c0316e1b7bf0a6ae1b6ca7f
Ivana <ivana_pile@yahoo.com>
2020-07-06 21:25:23 -0400

    Done.

---
+++ build/precache-manifest.975bff8a4661a43529b8981a8b4dedd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c8c9b2b74e2ece2de113bc59cfead7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15fb645d9bd6df9918c4",
+    "url": "/static/js/2.7ac62fa9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.7ac62fa9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d3c93f2b3fd29baab11",
+    "url": "/static/js/main.a47727cb.chunk.js"
+  },
+  {
+    "revision": "b6b7d9f341d802461e6f",
+    "url": "/static/js/runtime-main.b2d6c529.js"
+  }
+]);
